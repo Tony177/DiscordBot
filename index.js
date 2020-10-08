2cf6bea11073e7b0948303f603fca9126d68577e
Antonio <antonioavolio18@gmail.com>
2020-10-08 23:43:31 +0200

    Improved play function
    Generalized play function, now it can work with every mp3 file from the main directory.

--- index.js
+++ index.js
@@ -19,32 +19,36 @@ client.once('ready', () => {
 
 // Ready to log any message and author, ON instead of ONCE to do it always
 client.on('message', message => {
-	console.log((message.author.username).concat(':  ',message.content));
-
+	console.log(message.author.username + ': ' + message.content);
 });
 
 
 	client.on('message', async message => {
-		// Voice only works in guilds, ignoring the message otherwise
-		if (!message.guild)	return;
-	  
-		if (message.content === '!play') {
+		// Voice only works in guilds and with the right sintax, exting from the function otherwise
+		if (!message.guild || !message.content.includes(prefix + 'play')) return;
+
+		//Cutting the prefix and the keyword "play" from the initial message
+		const argoment = message.content.slice(prefix.length+4).trim().split(/ +/);
+		const command = argoment.shift().toLowerCase();
+		console.log(command+'\n');
+		 {
 		  // Only try to join the sender's voice channel if they are in one themselves
 		  if (message.member.voice.channel) {
 			try{
+				//Joining voice channel and waiting for the command 
 				const connection=message.member.voice.channel.join();
-				const dispatcher = (await connection).play('audio.mp3');
+				const dispatcher = (await connection).play(command + '.mp3');
 
 						//Console feedback on start and stop
 				dispatcher.on('start', () => {
-				console.log('audio.mp3 is now playing!');
+				console.log(command + ' is now playing!');
 
 				});
-				if(message.content === '!stop')
+				if(message.content === '${prefix}stop')
 					(await connection).disconnect();
 					
 				dispatcher.on('finish', () => {
-				console.log('audio.mp3 finished playing!');
+				console.log(command +' has finished playing!');
 
 				});
 				// Error Handling
